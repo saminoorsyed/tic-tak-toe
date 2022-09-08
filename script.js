c2a94d9bddb9b3f7a1a00c7ece8b2ba23913a7e5
saminoorsyed <saminoorsyed@gmail.com>
2022-09-08 19:00:42 -0400

    Finished basic logic + check for winner
    debugged canvas draw. each new draw needs a beginPath() statement
    clearRect() method has a large load. need to be careful in the future

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const spaces = document.querySelectorAll('.draw')
 const chooseO = document.querySelector('.playerO')
 
 let moves = []
+let playerXMoves = []
+let playerOMoves = []
+
 // flags
 // true means X turn, false means O turn
 let playerFlag = true;
@@ -13,6 +16,7 @@ drawMove = (move) => {
     const canvas = move
     const ctx = canvas.getContext("2d");
     if (playerFlag){
+        ctx.beginPath();
         ctx.moveTo(0,0);
         ctx.lineTo(100,100);
         ctx.moveTo(100,0);
@@ -42,6 +46,7 @@ computerMove = () => {
         move = Math.round((Math.random()*9)+.49)
     }
     moves.push(move)
+    pushPlayerMove(move)
     // node list index starts at 0, so we need to subtract 1
     const CpMove = spaces[move-1]
     drawMove(CpMove)
@@ -53,21 +58,58 @@ playerMove = (e)=> {
     if (moves.includes(move)) return
     moves.push(move);
     drawMove(e.target)
+    pushPlayerMove(move)
     playerFlag = !playerFlag
-    setTimeout(()=>{
-        computerMove()
-        playerFlag = !playerFlag
-    },100)
+    computerMove()
+    playerFlag = !playerFlag
+}
+
+pushPlayerMove = (move)=>{
+    playerFlag ? player = playerXMoves.push(move) : player = playerOMoves.push(move)
+    console.log(checkWin())
 }
 
 restart = () => {
-    console.log ('hello')
     spaces.forEach((space) => {
         ctx = space.getContext('2d')
         ctx.clearRect(0,0,100,100)
     })
+    // reset logged moves
     moves = []
+    playerXMoves = []
+    playerOMoves = []
+    playerFlag = true
+    startGameFlag = false
 }
 
+// check the rows, columns and diagonals
+checkWin = () => checkRows() || checkCols() || checkDiag()
+
+checkRows =()=>{
+    let player;
+    playerFlag ? player = playerXMoves : player = playerOMoves
+    for (let i=1; i<=7; i+=3){
+        if (player.includes(i) && player.includes(i+1) && player.includes(i+2)){
+            return true
+        }
+    }
+    return false
+    
+}
+checkCols =()=>{
+    let player;
+    playerFlag ? player = playerXMoves : player = playerOMoves 
+    for (let i=1; i<=3; i++){
+        if (player.includes(i) && player.includes(i+3) && player.includes(i+6)) return true
+    }
+    return false
+}
+checkDiag =()=>{
+    let player;
+    playerFlag ? player = playerXMoves : player = playerOMoves 
+    if (player.includes(1) && player.includes(5) && player.includes(9)) return true
+    if (player.includes(3) && player.includes(5) && player.includes(7)) return true
+    return false
+}
 spaces.forEach(space => space.addEventListener('click', playerMove));
 chooseO.addEventListener('click', playTurnX)
