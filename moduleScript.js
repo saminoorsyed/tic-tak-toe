950a515ae495d04be6d39166038acfb7e0eef798
saminoorsyed <saminoorsyed@gmail.com>
2022-09-09 16:41:57 -0400

    Implement Cp module + player factory function

--- moduleScript.js
+++ moduleScript.js
@@ -15,7 +15,9 @@ const gameBoard=(()=>{
     const getPlayerMoves = (playerFlag) => {
         return playerFlag ? _playerXMoves : _playerOMoves;
     }
-
+    const getAllMoves = ()=>{
+        return _playerOMoves.concat(_playerXMoves)
+    }
     const reset = ()=> {
         _playerXMoves = [];
         _playerOMoves = [];
@@ -24,7 +26,60 @@ const gameBoard=(()=>{
     return {
         pushPlayerMove,
         getPlayerMoves,
+        getAllMoves,
         reset,
     }
-})
+})();
 
+// factory function that maintains the player flag and combines that to push the player move to GameBoard
+const Player = (playerFlag)=>{
+    
+    const makeMove = (move) => {
+        gameBoard.pushPlayerMove(playerFlag, move)
+    }
+
+    return {
+        makeMove,
+    }
+}
+
+// module to handle computer moves
+const Computer = (()=>{
+    'use strict';
+    
+    let difficulty;
+    
+    const setDifficulty = (gameMode) => {
+        difficulty = gameMode;
+    }
+    const chooseMove = () => {
+        if (difficulty === 'easy'){
+            return chooseRand();
+        }else if (difficulty === 'medium'){
+            return chooseMedium();
+        }else {
+            return chooseUnbeatable();
+        }
+    }
+
+    const chooseRand =() =>{
+        let move = 0
+        const moves = gameBoard.getAllMoves()
+        while (moves.includes(move)|| move === 0){
+            move = Math.round((Math.random()*9)+.49)
+        }
+        return move
+    }
+
+    const chooseMedium =() =>{
+        return
+    }
+
+    const chooseUnbeatable =() =>{
+        return
+    }
+
+    return{
+        chooseMove
+    }
+})();
